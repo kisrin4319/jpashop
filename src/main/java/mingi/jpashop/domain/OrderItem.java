dc2da760acfa3fe95e29af3e6074c605dacb3d17
kisrin4319 <kisrin4319@gmail.com>
2024-11-10 16:32:50 +0900

    feat: Section 9 // 프록시와 연관관계 정리

--- src/main/java/mingi/jpashop/domain/OrderItem.java
+++ src/main/java/mingi/jpashop/domain/OrderItem.java
@@ -2,6 +2,8 @@ package mingi.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 public class OrderItem {
 
@@ -10,11 +12,11 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 

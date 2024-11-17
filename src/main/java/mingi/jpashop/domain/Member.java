7521b2ff4eb23002022c35eb2d4862ce17340db1
kisrin4319 <kisrin4319@gmail.com>
2024-11-17 16:41:05 +0900

    feat: Section 10 // 값 타입

--- src/main/java/mingi/jpashop/domain/Member.java
+++ src/main/java/mingi/jpashop/domain/Member.java
@@ -13,9 +13,10 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
+
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<Order>();
@@ -36,28 +37,19 @@ public class Member {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
-    public String getStreet() {
-        return street;
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
 }
